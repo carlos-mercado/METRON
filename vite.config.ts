0944cf4c682c1eaf0b8bf76cde2f23fea20a063e
carlos-mercado <2.carlosmercado@gmail.com>
2025-09-06 15:04:24 -0700

    Updated base path for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/METRON/',
+  base: '/',
   plugins: [
     react(),
     viteStaticCopy({
