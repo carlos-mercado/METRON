4ac0a99d276456ea2816adb1b19c02f9064a1126
Carlos Mercado <2.carlosmercado@gmail.com>
2025-12-19 22:36:28 -0800

    changed interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 import { getAuth, signOut } from "firebase/auth";
-import { useState } from 'react';
-import logo from './assets/logo.png';
+//import { useState } from 'react';
+import alt_logo from './assets/alt_logo.svg';
 import footer from './assets/footer.png';
 import EditWorkouts from './EditWorkouts';
 import CreateWorkout from './CreateWorkout'
-import StartWorkout from './StartWorkout'
 
 interface AppProps
 {
@@ -14,74 +13,57 @@ interface AppProps
 }
 
 
-function App(props: AppProps) {
-  const navigate = useNavigate();
-  const [workoutKey, setWorkoutKey] = useState("");
-  const [view, setView] = useState(false);
+function App(props: AppProps) 
+{
+    const navigate = useNavigate();
 
-  let handleLogout = async () =>
-  {
-    try {
-      await signOut(getAuth());
-      // optional: reset UI state
-      setView(false);
-      props.callback(false);
-    } catch (err) {
-      console.error('Logout failed', err);
-      alert('Logout failed');
+    let handleLogout = async () =>
+    {
+        try {
+            await signOut(getAuth());
+            props.callback(false);
+        } catch (err) {
+            console.error('Logout failed', err);
+            alert('Logout failed');
+        }
     }
-  }
 
-  return (
-    <>
-    <header className="mainHeader">
-        <a href="/">
-            <img id='logo' src={logo} alt="Logo"/>
-        </a>
-    </header>
-    <Routes>
-        <Route path="/" element=
-        {
-          <>
-            <div className='mainContent'>
-              <button className="mainButtons" onClick={() => {
-                navigate('/create-workout');
-              }}>✚</button>
-              <button className="mainButtons" onClick={() => {
-                setView(!view);
-              }}>▶</button>
-              <button className="editButton" onClick={() => {
-                navigate('/edit-workouts');
-                setView(false);
-              }}>▶</button>
-              <button className="logoutButton" onClick={() => {
-                setView(!view);
-                handleLogout();
-              }}>▶</button>
+    return (
+        <>
+        <header className="mainHeader">
+            <a href="/">
+                <img id='logo' src={alt_logo} alt="Logo"/>
+            </a>
+        </header>
+        <Routes>
+            <Route path="/" element=
+            {
+                <>
+                <div className='mainContent'>
+                    <button className="mainButtons" onClick={() => {
+                    navigate('/create-workout');
+                    }}>✚</button>
+                    <button className="editButton" onClick={() => {
+                    navigate('/edit-workouts');
+                    }}>▶︎</button>
+                    <button className="logoutButton" onClick={() => {
+                    handleLogout();
+                    }}>▶</button>
 
-            </div>
-            {view &&
-              <div className='startWorkoutForm'>
-                <input className='keyInput' placeholder='Enter workout key...' onChange={(e) => setWorkoutKey(e.target.value)}></input>
-                <button className="goButton" onClick={() => {
-                  navigate(`/start-workout/${workoutKey}`); 
-                }}>GO</button>
-              </div>
-            }
-          </>
-        } 
-        />
-        <Route path="/create-workout/" element={<CreateWorkout/>} />
-        <Route path="/start-workout/:workoutKey" element={<StartWorkout/>} />
-        <Route path="/edit-workouts" element={<EditWorkouts/>} />
-      </Routes>
+                </div>
+                </>
+            } 
+            />
+            <Route path="/create-workout/" element={<CreateWorkout/>} />
+            <Route path="/edit-workouts" element={<EditWorkouts/>} />
+            </Routes>
 
-      <footer className="appFooter">
-        <img src={footer} alt="footer" />
-      </footer>
+            <footer className="appFooter">
+            <img src={footer} alt="footer" />
+            </footer>
 
-    </>
-  )
+        </>
+    )
 }
 
 export default App
